15c20c697ea081b4e80472b49716455c1a112401
MEHEDIs PC <miraz.onesoft@gmail.com>
2022-08-26 23:12:14 +0600

    init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const data = require("./db.js");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(data);
-const middlewares = jsonServer.defaults({ noCors: true });
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
 server.use(middlewares);
