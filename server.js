0737d18875bdc087db3cea07f12ecf96f03d96b6
MEHEDIs PC <miraz.onesoft@gmail.com>
2022-08-26 23:16:27 +0600

    ok

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
 server.use(middlewares);
-server.use("https://venerable-kulfi-5fd630.netlify.app", router);
+server.use(router);
 
 server.listen(port);
